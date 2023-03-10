307066ebf3c1175412b0a8bfaef3a5a2bac01f20
MustafaMertSandal <78301079+MustafaMertSandal@users.noreply.github.com>
2023-03-10 15:27:50 +0300

    Add files via upload

--- http_request/post_request.dart
+++ http_request/post_request.dart
@@ -1,15 +1,26 @@
 import 'dart:convert';
-
+import 'dart:math';
 import 'package:http/http.dart' as http;
 
 class PostRequest {
-  postData(double temp, bool detected, String url) {
-    return http.post(Uri.parse(url),
+  postTemperatureData(double temp, String tempUrl) {
+    double randomTemp = (temp - 2) + 4 * Random().nextDouble();
+    randomTemp = double.parse(randomTemp.toStringAsFixed(2));
+    return http.post(Uri.parse(tempUrl),
         headers: <String, String>{
           'Content-Type': 'application/json',
         },
         body: jsonEncode({
-          "temperature": temp,
+          "temperature": randomTemp,
+        }));
+  }
+
+  postDetectionData(bool detected, String detectionUrl) {
+    return http.post(Uri.parse(detectionUrl),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode({
           "human detection": detected,
         }));
   }
