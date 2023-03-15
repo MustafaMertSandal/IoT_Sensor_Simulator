09406605e7a7f6975da6d56532e081f75474496b
MustafaMertSandal <78301079+MustafaMertSandal@users.noreply.github.com>
2023-03-15 10:59:09 +0300

    Add files via upload

--- http_request/post_request.dart
+++ http_request/post_request.dart
@@ -3,8 +3,9 @@ import 'dart:math';
 import 'package:http/http.dart' as http;
 
 class PostRequest {
-  postTemperatureData(double temp, String tempUrl) {
-    double randomTemp = (temp - 2) + 4 * Random().nextDouble();
+  postTemperatureData(int temp, String tempUrl, int variance) {
+    double randomTemp =
+        (temp - variance) + variance * 2 * Random().nextDouble();
     randomTemp = double.parse(randomTemp.toStringAsFixed(2));
     return http.post(Uri.parse(tempUrl),
         headers: <String, String>{
